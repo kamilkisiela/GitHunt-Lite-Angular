3c00480ae2a28c2ade06b4aecb6b2826f1256d5e
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 13:57:30 +0200

    44. Add a logic with a template to NewComponent

--- src/app/new-comment/new-comment.component.ts
+++ src/app/new-comment/new-comment.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-comment',
   templateUrl: './new-comment.component.html',
   styleUrls: ['./new-comment.component.scss']
 })
-export class NewCommentComponent implements OnInit {
+export class NewCommentComponent {
+  comment: string;
 
   constructor() { }
 
-  ngOnInit() {
+  submit() {
+    if (this.comment && this.comment.length > 0) {
+      // an action
+    }
   }
 
 }
