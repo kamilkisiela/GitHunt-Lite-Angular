e88ed1a664bb8af5bf438d79e5ad76f971a19957
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 12:09:53 +0200

    7. Use RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FeedComponent } from './feed/feed.component';
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { FeedComponent } from './feed/feed.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
