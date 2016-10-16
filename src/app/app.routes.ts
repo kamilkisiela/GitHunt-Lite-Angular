0c312775b04765c17f38e70c26bb4de4d9c87101
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 13:50:25 +0200

    40. Define a route for CommentsPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Route } from '@angular/router';
 
 import { FeedComponent } from './feed/feed.component';
 import { NewEntryComponent } from './new-entry/new-entry.component';
+import { CommentsPageComponent } from './comments-page/comments-page.component';
 
 export const routes: Route[] = [
   { path: '', component: FeedComponent },
-  { path: 'submit', component: NewEntryComponent }
+  { path: 'submit', component: NewEntryComponent },
+  { path: ':owner/:name', component: CommentsPageComponent } // angular/angularjs
 ];
