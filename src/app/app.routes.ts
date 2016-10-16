65f0c206dc2ce3b3bdfe660c18ed3c50fdb85f5a
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 13:11:44 +0200

    23. Define /submit route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Route } from '@angular/router';
 
 import { FeedComponent } from './feed/feed.component';
+import { NewEntryComponent } from './new-entry/new-entry.component';
 
 export const routes: Route[] = [
-  { path: '', component: FeedComponent }
+  { path: '', component: FeedComponent },
+  { path: 'submit', component: NewEntryComponent }
 ];
