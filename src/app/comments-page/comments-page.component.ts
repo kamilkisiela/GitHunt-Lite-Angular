2ec3d060f8412bac9b32b7b75ebd7a33a9d79571
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 14:01:35 +0200

    46. Prepare CommentsPage for submiting a new comments

--- src/app/comments-page/comments-page.component.ts
+++ src/app/comments-page/comments-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OnSubmitEvent } from '../new-comment/new-comment.models';
+
 @Component({
   selector: 'app-comments-page',
   templateUrl: './comments-page.component.html',
@@ -10,6 +12,10 @@ export class CommentsPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(event: OnSubmitEvent) {
+    // action
   }
 
 }
