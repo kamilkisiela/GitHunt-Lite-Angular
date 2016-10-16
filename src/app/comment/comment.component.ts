9cae32ff5de4221e337e0cfd96f5ebd0978fc935
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 14:15:40 +0200

    52. Add a template and few bindings to present data

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss']
 })
-export class CommentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CommentComponent {
+  @Input() content: string;
+  @Input() createdAt: Date;
+  @Input() author: string;
 }
