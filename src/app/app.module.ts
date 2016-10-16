9c4b52468556ed93b006a004482b193c0325ce89
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 12:44:25 +0200

    11. Use ApolloModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ApolloModule } from 'angular2-apollo';
 
 import { AppComponent } from './app.component';
+import { client } from './apollo';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FeedComponent } from './feed/feed.component';
 import { routes } from './app.routes';
@@ -17,7 +19,8 @@ import { routes } from './app.routes';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ApolloModule.withClient(client)
   ],
   providers: [],
   bootstrap: [AppComponent]
